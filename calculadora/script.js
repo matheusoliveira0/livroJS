c2da1568906c0f49568d86f2392498b0e3fc37c6
Matheus Oliveira <teumatheus.73@gmail.com>
2026-09-01 20:34:36 -0300

    feat: calculadora e efeitoDigitação
    finalizei o projeto calculadora, comecei e finalizei o projeto efeitoDigitação

--- calculadora/script.js
+++ calculadora/script.js
@@ -24,34 +24,66 @@ function addDigit(digit) {
 
 function setOperator(newOperator) {
   if (currentNumber) {
+    calculate();
+
     firstOperand = parseFloat(currentNumber.replace(",", "."));
     currentNumber = "";
   }
   operator = newOperator;
 }
 function calculate() {
-  if (oerator == nul || firstOperand == null) return;
+  if (operator == null || firstOperand == null) return;
   let secondOperand = parseFloat(currentNumber.replace(",", "."));
   let resultValue;
+  switch (operator) {
+    case "+":
+      resultValue = firstOperand + secondOperand;
+      break;
+    case "-":
+      resultValue = firstOperand - secondOperand;
+      break;
+    case "x":
+      resultValue = firstOperand * secondOperand;
+      break;
+    case "/":
+      resultValue = firstOperand / secondOperand;
+      break;
+    default:
+      return;
+  }
+
+  if (resultValue.toString().split(".")[1]?.length > 5) {
+    currentNumber = parseFloat(resultValue.toFixed(5)).toString();
+  } else {
+    currentNumber = resultValue.toString();
+  }
+
+  operator = null;
+  firstOperand = null;
+  restart = true;
+  percentageValuue = null;
+  updateResult();
 }
 
-switch (operator) {
-  case "+":
-    resultValue = firstOperand + secondOperand;
-    break;
-  case "-":
-    resultValue = firstOperand - secondOperand;
-    break;
-  case "*":
-    resultValue = firstOperand * secondOperand;
-    break;
-  case "/":
-    resultValue = firstOperand / secondOperand;
-    break;
-  default:
-    return;
+function clearCalculator() {
+  currentNumber = "";
+  firstOperand = null;
+  operator = null;
+  updateResult(true);
 }
+function setPercentage() {
+  let result = parseFloat(currentNumber) / 100;
 
+  if (["+", "-"].includes(operator)) {
+    result = result * (firstOperand || 1);
+  }
+  if (result.toString().split(".")[1]?.length > 5) {
+    result = result.toFixed(5).toString();
+  }
+
+  currentNumber = result.toString();
+  updateResult();
+}
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     const buttonText = button.innerText;
@@ -59,6 +91,17 @@ buttons.forEach((button) => {
       addDigit(buttonText);
     } else if (["+", "-", "x", "/"].includes(buttonText)) {
       setOperator(buttonText);
+    } else if (buttonText == "=") {
+      calculate();
+    } else if (buttonText == "C") {
+      clearCalculator();
+    } else if (buttonText == "±") {
+      currentNumber = (
+        parseFloat(currentNumber || firstOperand) * -1
+      ).toString();
+      updateResult();
+    } else if (buttonText == "%") {
+      setPercentage();
     }
   });
 });
